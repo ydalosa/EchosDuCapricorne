f7bf90c5df83b7b7df45dc1e792f8e352211f8fb
Andy <96415472+ydalosa@users.noreply.github.com>
2022-08-04 20:17:08 +0200

    first commit

---
+++ main.js
@@ -0,0 +1,7 @@
+const Bouton = document.getElementById('bouton');
+Bouton.addEventListener('click', (e) => {
+   e.preventDefault();
+   console.log('nisy');
+   Bouton.value="bt"
+   bouton.type='button'
+})
